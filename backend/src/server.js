8943e2a9e79e53b443933a7036397157eedf942e
sobhi69 <mhegazy6371@gmail.com>
2025-05-22 19:18:27 +0300

    lib

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const connectToDB_1 = require("./lib/connectToDB");
+const connectToDB_1 = require("./lib/connectToDb");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
